45b58e9f68a1ef7223720be3d0a8c9b85b398189
zachseibers <zachseibers@gmail.com>
2017-02-19 16:14:43 -0500

    Initial commit

---
+++ Test1/Test1/Test1.py
@@ -0,0 +1,11 @@
+# Tool for calculating remaining mileage on a lease car
+
+print("Hello World")
+
+MilesPerDay = 82
+DaysPerYear = 365
+MilesPerYear = MilesPerDay*DaysPerYear
+print("For", MilesPerDay,"miles per day, there is a total of" ,MilesPerYear,"miles in a calendar year.")
+
+
+
