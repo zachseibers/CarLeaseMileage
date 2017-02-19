bc0982813e6b80bd6cd0f057d26172287822906f
zachseibers <zachseibers@gmail.com>
2017-02-19 16:26:11 -0500

    Added number of days calculator

--- Test1/Test1/Test1.py
+++ Test1/Test1/Test1.py
@@ -7,5 +7,12 @@ DaysPerYear = 365
 MilesPerYear = MilesPerDay*DaysPerYear
 print("For", MilesPerDay,"miles per day, there is a total of" ,MilesPerYear,"miles in a calendar year.")
 
+from datetime import date
+PickupDate = date(2016, 4, 8)
+DueDate=date(2017, 4, 8)
+#default date input form is yyyy, mo, dd; can change using date_format = "%m/%d/%Y"
+CurrentDate = date(2017, 2, 19)
+DaysRemaining = DueDate - CurrentDate
+
 
 
